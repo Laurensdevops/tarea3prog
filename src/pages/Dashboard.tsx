7bdd464dcebb99baa2b88d0e60ff256a4055446e
Laurensdevops <Claurencio@laurensst.com>
2025-08-01 16:34:50 -0400

    cambio de textos 2

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -60,7 +60,7 @@ const Dashboard = () => {
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-8">
           <Card>
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">Total Items</CardTitle>
+              <CardTitle className="text-sm font-medium">Total Items Disponible</CardTitle>
               <Package className="h-4 w-4 text-muted-foreground" />
             </CardHeader>
             <CardContent>
